8dadef9e15f79fd27243678067e8574e0da885f8
Matilda Mared <matildamared@live.se>
2021-12-11 15:39:46 +0100

    complete section 2

--- section-2/app.js
+++ section-2/app.js
@@ -1,16 +1,11 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Hello";
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log("Result: ", num);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-var combineValues;
-combineValues = add;
-// combineValues = printResult;
-console.log(combineValues(7, 5));
-printResult(add(5, 13));
-addAndHandle(5, 3, function (num) { return console.log("hello", num); });
+var result = generateError("An error occured", 500);
