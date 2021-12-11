3b54444b66f6182684045cb0df38b7b2bdceecde
Matilda Mared <matildamared@live.se>
2021-12-11 15:05:49 +0100

    work with union aliases

--- section-2/app.js
+++ section-2/app.js
@@ -1,6 +1,22 @@
-function combine(input1, input2) {
-    var result = input1 + input2;
+function combine(input1, input2, resultConversion) {
+    var result;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversion === "as-number") {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    // if (resultConversion === "as-number") {
+    // 	return +result;
+    // } else {
+    // 	return result.toString();
+    // }
     return result;
 }
-var combinedAges = combine(30, 20);
+var combinedAges = combine(30, 20, "as-number");
 console.log(combinedAges);
+var combinedStringAges = combine("30", "20", "as-number");
+console.log(combinedStringAges);
+var combinedNames = combine("Max", "Anna", "as-string");
+console.log(combinedNames);
