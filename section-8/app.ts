4e1a154a0077b1271cda9d357aaa8e7130424ec5
Matilda Mared <matildamared@live.se>
2021-12-13 09:39:04 +0100

    begin working on section 8

---
+++ section-8/app.ts
@@ -0,0 +1,164 @@
+function Logger(logString: string) {
+	console.log("Logger factory");
+	return function (constructor: Function) {
+		console.log(logString);
+		console.log(constructor);
+	};
+}
+
+function WithTemplate(template: string, hookId: string) {
+	console.log("Template factory");
+	return function <T extends { new (...args: any[]): { name: string } }>(
+		originalConstructor: T
+	) {
+		return class extends originalConstructor {
+			constructor(...args: any[]) {
+				super();
+				console.log("Rendering template");
+				const hookElement = document.getElementById(hookId);
+				if (hookElement) {
+					hookElement.innerHTML = template;
+					hookElement.querySelector("h1")!.textContent = this.name;
+				}
+			}
+		};
+	};
+}
+
+// @Logger("Logging Person")
+@Logger("Logging")
+@WithTemplate("<h1>My Person Object</h1>", "app")
+class Person {
+	name = "Tilda";
+
+	constructor() {
+		console.log("Creating new person object...");
+	}
+}
+
+const pers = new Person();
+console.log(pers);
+
+function Log(target: any, propertyName: string | Symbol) {
+	console.log("Property decorator!");
+	console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+	console.log("Accessor decorator!");
+	console.log(target);
+	console.log(name);
+	console.log(descriptor);
+}
+
+function Log3(
+	target: any,
+	name: string | Symbol,
+	descriptor: PropertyDescriptor
+) {
+	console.log("Method decorator!");
+	console.log(target);
+	console.log(name);
+	console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+	console.log("Parameter decorator!");
+	console.log(target);
+	console.log(name);
+	console.log(position);
+}
+
+class Product {
+	@Log
+	title: string;
+	private _price: number;
+
+	@Log2
+	set price(val: number) {
+		if (val > 0) {
+			this._price = val;
+		} else {
+			throw new Error("Invalid price, must be a positive number");
+		}
+	}
+
+	constructor(t: string, p: number) {
+		this.title = t;
+		this._price = p;
+	}
+
+	@Log3
+	getPriceWithTax(@Log4 tax: number) {
+		return this._price * (1 + tax);
+	}
+}
+
+function Autobind(
+	target: any,
+	methodName: string | Symbol | number,
+	descriptor: PropertyDescriptor
+) {
+	const originalMethod = descriptor.value;
+	const adjDescriptor: PropertyDescriptor = {
+		configurable: true,
+		enumerable: false,
+		get() {
+			const boundFn = originalMethod.bind(this);
+			return boundFn;
+		},
+	};
+	return adjDescriptor;
+}
+
+class Printer {
+	message = "This works";
+
+	@Autobind
+	showMessage() {
+		console.log(this.message);
+	}
+}
+
+const p = new Printer();
+
+const button = document.querySelector("button")!;
+button.addEventListener("click", p.showMessage);
+
+p.showMessage();
+
+function Required() { }
+
+function PositiveNumber() { }
+
+function validate(obj: object) {}
+
+class Course {
+  @Required
+  title: string;
+  @PositiveNumber
+	price: number;
+
+	constructor(t: string, p: number) {
+		this.title = t;
+		this.price = p;
+	}
+}
+
+const courseForm = document.querySelector("form")!;
+courseForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const titleElement = document.getElementById("title") as HTMLInputElement;
+	const priceElement = document.getElementById("price") as HTMLInputElement;
+
+	const title = titleElement.value;
+	const price = +priceElement.value;
+
+  const createdCourse = new Course(title, price);
+  
+  if (!validate(createdCourse)) {
+    alert("Error");
+  }
+
+	console.log(createdCourse);
+});
