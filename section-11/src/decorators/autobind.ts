8663891aa204786da3023f6646429601ad6c8b12
Matilda Mared <matildamared@live.se>
2022-03-20 10:29:47 +0100

    add webpack and entry/output configuration

---
+++ section-11/src/decorators/autobind.ts
@@ -0,0 +1,17 @@
+
+	// Autobind Decorator
+	export function autobind(
+		target: any,
+		methodName: string,
+		descriptor: PropertyDescriptor
+	) {
+		const originalMethod = descriptor.value;
+		const adjustedDescriptor: PropertyDescriptor = {
+			configurable: true,
+			get() {
+				const boundFunction = originalMethod.bind(this);
+				return boundFunction;
+			},
+		};
+		return adjustedDescriptor;
+	}
