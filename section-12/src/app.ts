c5336856b450a56c5a03071629f427d52a8c4337
Matilda Mared <matildamared@live.se>
2022-03-21 14:22:00 +0100

    practice with class-validator

--- section-12/src/app.ts
+++ section-12/src/app.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import { plainToClass } from "class-transformer";
 import { Product } from "./product.model";
+import { validate } from "class-validator";
 
 const products = [
 	{ title: "A carpet", price: 29.99 },
@@ -11,11 +12,22 @@ const products = [
 const p1 = new Product("A book", 12.99);
 console.log(p1.getInformation());
 
-const loadedProducts = plainToClass(Product, products);
+const newProd = new Product("", -12.99);
+validate(newProd).then((errors) => {
+	if (errors.length > 0) {
+		console.log("Validation errors!");
+		console.log(errors);
+	} else {
+		console.log(newProd);
+	}
+});
+console.log(newProd.getInformation());
 
 // const loadedProducts = products.map((product) => {
 // 	return new Product(product.title, product.price);
 // });
+
+const loadedProducts = plainToClass(Product, products);
 
 for (const product of loadedProducts) {
 	console.log(product.getInformation());
