6f2d18c37afa726bec634cecf5ed7db4f31c3b75
Matilda Mared <matildamared@live.se>
2022-03-21 14:14:30 +0100

    work with class-transformer package

--- section-12/src/app.ts
+++ section-12/src/app.ts
@@ -1,3 +1,22 @@
-import _ from "lodash";
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
+import { Product } from "./product.model";
 
-console.log(_.shuffle([1, 2, 3]));
+const products = [
+	{ title: "A carpet", price: 29.99 },
+	{ title: "A pillow", price: 9.99 },
+	{ title: "A blanket", price: 15.99 },
+];
+
+const p1 = new Product("A book", 12.99);
+console.log(p1.getInformation());
+
+const loadedProducts = plainToClass(Product, products);
+
+// const loadedProducts = products.map((product) => {
+// 	return new Product(product.title, product.price);
+// });
+
+for (const product of loadedProducts) {
+	console.log(product.getInformation());
+}
