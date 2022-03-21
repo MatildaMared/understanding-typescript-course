bcbebfd58c3e59f1fb4b62d021c51f5f12b664f0
Matilda Mared <matildamared@live.se>
2022-03-21 16:22:41 +0100

    initialize section 14

--- section-13/src/app.ts
+++ section-13/src/app.ts
@@ -1,1 +1,43 @@
-// Code goes here!
+import axios from "axios";
+const { API_KEY } = require("./apikey");
+const form = document.querySelector("form");
+const addressInput = document.querySelector("#address") as HTMLInputElement;
+
+type GoogleGeoCodingResponse = {
+	results: { geometry: { location: { lat: number; lng: number } } }[];
+	status: "OK" | "ZERO_RESULTS";
+};
+
+function searchAddressHandler(event: Event) {
+	event.preventDefault();
+
+	const enteredAddress = addressInput.value;
+	console.log(enteredAddress);
+
+	// send to google api
+	axios
+		.get<GoogleGeoCodingResponse>(
+			`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+				enteredAddress
+			)}&key=${API_KEY}`
+		)
+		.then((response) => {
+			if (response.data.status !== "OK") {
+				throw new Error("Could not fetch location!");
+			}
+			const coordinates = response.data.results[0].geometry.location;
+			const map = new google.maps.Map(document.getElementById("map")!, {
+				center: coordinates,
+				zoom: 16,
+			});
+			new google.maps.Marker({
+				position: coordinates,
+				map: map,
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+}
+
+form?.addEventListener("submit", searchAddressHandler);
